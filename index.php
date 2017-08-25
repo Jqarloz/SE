b780a023ed86df7325e854398f4c16f661494246
Jqarloz <juancarlos_hs@hotmail.com>
2017-08-25 15:39:56 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+?>
+<html>
+	<head>
+		<title>.: RLPHP :.</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	</head>
+	<body>
+	<?php include "php/navbar.php"; ?>
+	<div class="container">
+	<div class="row">
+	<div class="col-md-12">
+			<h2 align="center">REGISTRO Y LOGIN SIIE</h2>
+			<p class="lead" align="center">Sistema de registro y login SIIE</p>
+			<p align="center">Instrucciones:</p>
+			
+				<li align="center">Registrate en la opcion de registro.</li>
+				<li align="center">Inicie sesion en la opcion de login.</li>
+				<li align="center">Para finalizar la sesion, click en la opcion salir .</li>
+	</div>
+	</div>
+	</div>
+	</body>
+</html>
